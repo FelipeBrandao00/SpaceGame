63d98918a7041f1ab320c0aad706225f1acc5be1
FelipeBrandao00 <felipesbrandao13@gmail.com>
2024-09-17 21:15:59 -0300

    Prender nave na tela

--- Assets/Scripts/SpaceShip.cs
+++ Assets/Scripts/SpaceShip.cs
@@ -3,12 +3,37 @@ using UnityEngine;
 public class SpaceShip : MonoBehaviour
 {
     public Projectile ProjectilePrefab;
-    public BoxCollider2D colider;
     public float speed = 5f;
+    private SpriteRenderer _spriteRenderer;
+    private Bounds _CameraBounds;
+
+    private void Start()
+    {
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+
+        var height = Camera.main.orthographicSize * 2;
+        var width = height * Camera.main.aspect;
+
+        _CameraBounds = new Bounds(Vector3.zero,new Vector3(width,height));
+    }
     void Update()
     {
         MoverNave();
         Atirar();
+    }
+
+    private void LateUpdate()
+    {
+        var novaPosicao = transform.position;
+
+        var spriteWidht = _spriteRenderer.sprite.bounds.extents.x;
+        var spriteHeight = _spriteRenderer.sprite.bounds.extents.y;
+
+        novaPosicao.x = Mathf.Clamp(novaPosicao.x, _CameraBounds.min.x + spriteWidht, _CameraBounds.max.x - spriteWidht);
+        novaPosicao.y = Mathf.Clamp(novaPosicao.y, _CameraBounds.min.y + spriteHeight, _CameraBounds.max.y - spriteHeight);
+
+        transform.position = novaPosicao;
+
     }
 
     private void MoverNave()
